d363854d9f5ab3d1491fd8080f720910e8533e6c
terceiro.com.br <andre@terceiro.com.br>
2019-02-23 12:56:35 -0300

    Add files via upload

---
+++ potenciometro.ino
@@ -0,0 +1,31 @@
+float s;
+const int seletor = A3; 
+const int Bar_Y2 = A5;
+const int Bar_R = A4;
+const int lm35 = A0;
+
+float ler_sensor() {
+  return analogRead(lm35);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("i");
+  pinMode(Bar_R, OUTPUT);
+  pinMode(Bar_Y2, OUTPUT);
+  pinMode(seletor, OUTPUT);
+  pinMode(lm35, INPUT);
+  digitalWrite(seletor, HIGH);
+}
+
+void loop() {
+  s = ler_sensor();
+  
+  if (s > 60) {
+    digitalWrite(Bar_R, HIGH);
+    digitalWrite(Bar_Y2, LOW);
+  } else {
+    digitalWrite(Bar_Y2, HIGH);
+    digitalWrite(Bar_R, LOW);
+  }
+}
