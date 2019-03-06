119002ded0a7bb6654860e67b57c703547e4bb45
terceiro.com.br <andre@terceiro.com.br>
2019-03-06 03:42:47 -0300

    Add files via upload

---
+++ pot.ino
@@ -0,0 +1,13 @@
+const int pot=A2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pot, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  double v1 = analogRead(pot);
+  Serial.println(v1);
+  delay(1000);
+}
